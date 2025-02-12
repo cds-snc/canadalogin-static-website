52f8eb51cf2185213f2efae51926a803d58dee18
Mahamoud Mohamoud <Mahamoud.Mohamoud@tbs-sct.gc.ca>
2025-02-12 23:18:18 +0000

    simplified the s3 bucket naming

--- terragrunt/aws/s3_cloudfront_simple.tf
+++ terragrunt/aws/s3_cloudfront_simple.tf
@@ -2,7 +2,7 @@ module "gc_signin_zone_en_website" {
   source                  = "github.com/cds-snc/terraform-modules//simple_static_website?ref=v10.0.0"
   domain_name_source      = var.domain_en
   billing_tag_value       = var.billing_code
-  s3_bucket_name          = "${var.domain_en}-${var.env}"
+  s3_bucket_name          = var.domain_en
   force_destroy_s3_bucket = true
   index_document          = "index.html"
   hosted_zone_id          = aws_route53_zone.gc_signin_zone_en.zone_id
@@ -19,7 +19,7 @@ module "gc_signin_zone_fr_website" {
   source                  = "github.com/cds-snc/terraform-modules//simple_static_website?ref=v10.0.0"
   domain_name_source      = var.domain_fr
   billing_tag_value       = var.billing_code
-  s3_bucket_name          = "${var.domain_fr}-${var.env}"
+  s3_bucket_name          = var.domain_fr
   force_destroy_s3_bucket = true
   index_document          = "index.html"
   hosted_zone_id          = aws_route53_zone.gc_signin_zone_fr.zone_id
