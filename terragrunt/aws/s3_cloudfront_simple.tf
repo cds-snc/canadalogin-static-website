065d41c60de667bf7f76de00636b7bcac6592224
Mahamoud Mohamoud <Mahamoud.Mohamoud@tbs-sct.gc.ca>
2025-02-12 23:05:20 +0000

    added a french s3 bucket for the translated website

--- terragrunt/aws/s3_cloudfront_simple.tf
+++ terragrunt/aws/s3_cloudfront_simple.tf
@@ -1,11 +1,28 @@
-module "website" {
+module "gc_signin_zone_en_website" {
   source                  = "github.com/cds-snc/terraform-modules//simple_static_website?ref=v10.0.0"
   domain_name_source      = var.domain_en
   billing_tag_value       = var.billing_code
-  s3_bucket_name          = "${var.product_name}-${var.domain_en}-${var.env}"
+  s3_bucket_name          = "${var.domain_en}-${var.env}"
   force_destroy_s3_bucket = true
   index_document          = "index.html"
   hosted_zone_id          = aws_route53_zone.gc_signin_zone_en.zone_id
+  is_create_hosted_zone   = false
+
+  providers = {
+    aws           = aws
+    aws.dns       = aws # For scenarios where there is a dedicated DNS provder.
+    aws.us-east-1 = aws.us-east-1
+  }
+}
+
+module "gc_signin_zone_fr_website" {
+  source                  = "github.com/cds-snc/terraform-modules//simple_static_website?ref=v10.0.0"
+  domain_name_source      = var.domain_fr
+  billing_tag_value       = var.billing_code
+  s3_bucket_name          = "${var.domain_fr}-${var.env}"
+  force_destroy_s3_bucket = true
+  index_document          = "index.html"
+  hosted_zone_id          = aws_route53_zone.gc_signin_zone_fr.zone_id
   is_create_hosted_zone   = false
 
   providers = {
--- terragrunt/env/dev/env_vars.hcl
+++ terragrunt/env/dev/env_vars.hcl
@@ -1,10 +1,10 @@
 # Set the environment's global variables
 #
 inputs = {
-  account_id   = "767397971970"
-  domain_en    = "gc-signin.cdssandbox.xyz"
-  domain_fr    = "connexion-gc.cdssandbox.xyz"
+  account_id   = "058264382660"
+  domain_en    = "scratch-mm.gc-signin.cdssandbox.xyz"
+  domain_fr    = "scratch-mm.connexion-gc.cdssandbox.xyz"
   env          = "dev"
-  product_name = "gc-signin-website"
+  product_name = "gc-signin-website-1"
   region       = "ca-central-1"
 }
