02bebb21c0cbc8d8f65e22330b81bbde06f5e8b4
melodyhabb <melody.habbouche@cds-snc.ca>
2025-10-27 16:24:33 +0000

    Update contextual footer to be bilingual

--- website/src/_data/footer.js
+++ website/src/_data/footer.js
@@ -1,22 +1,40 @@
 module.exports = {
   en: {
+    contextual: {
+      heading: 'GC Sign in',
+      aboutUrl: '/en/partners/about-gc-sign-in/',
+      usersUrl: '/en/users/',
+      partnersUrl: '/en/partners/',
+      termsUsersUrl: '/en/users/terms/',
+      termsPartnersUrl: '/en/partners/terms-and-conditions/',
+      privacyPartnersUrl: '/en/partners/privacy/',
+    },
     sub: {
-      about: 'About GC Sign in',
-      abouturl: '/en/about-gc-sign-in/',
-      help: 'Help',
-      abouturl: '/en/',
-      terms: 'Terms',
-      termsurl: '/en/terms/'
+      about: 'About Canada.ca',
+      aboutUrl: 'https://design.canada.ca/about/',
+      terms: 'Terms and Conditions',
+      termsUrl: 'https://www.canada.ca/en/transparency/terms.html',
+      privacy: 'Privacy',
+      privacyUrl: 'https://www.canada.ca/en/transparency/privacy.html'
     }
   },
   fr: {
+    contextual: {
+      heading: 'Connexion GC',
+      aboutUrl: '/fr/partners/about-gc-sign-in/',
+      usersUrl: '/fr/users/',
+      partnersUrl: '/fr/partners/',
+      termsUsersUrl: '/fr/users/terms/',
+      termsPartnersUrl: '/fr/partners/terms-and-conditions/',
+      privacyPartnersUrl: '/fr/partners/privacy/',
+    },
     sub: {
-      about: 'About GC Sign in',
-      abouturl: '/fr/about-gc-sign-in/',
-      help: 'Help',
-      abouturl: '/fr/',
-      terms: 'Terms',
-      termsurl: '/fr/terms/'
+      about: 'À propos de Canada.ca',
+      aboutUrl: 'https://conception.canada.ca/a-propos/',
+      terms: 'Avis', 
+      termsUrl: 'https://www.canada.ca/fr/transparence/avis.html',
+      privacy: 'Confidentialité',
+      privacyUrl: 'https://www.canada.ca/fr/transparence/confidentialite.html'
     }
   },
 };
