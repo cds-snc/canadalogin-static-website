c75137b189b37f6599882253b8f80141c888b796
amalungo <afonso.malungo@cds-snc.ca>
2025-12-18 13:57:40 -0500

    Updated sub-links

--- website/src/_data/footer.js
+++ website/src/_data/footer.js
@@ -9,7 +9,7 @@ module.exports = {
       termsUsersUrl: '/en/users/terms/',
       termsPartnersUrl: '/en/partners/terms-and-conditions/',
       privacyPartnersUrl: '/en/partners/privacy/',
-      contactUsersUrl: '/en/users/contact-us/'
+      contactUsersUrl: '/en/users/contact-us/',
       contactPartnersUrl: 'https://forms-formulaires.alpha.canada.ca/en/id/cmfmxu3xh0000x401zperejwc'
     },
     sub: {
@@ -18,7 +18,11 @@ module.exports = {
       terms: 'Terms and conditions',
       termsUrl: 'https://www.canada.ca/en/transparency/terms.html',
       privacy: 'Privacy',
-      privacyUrl: 'https://www.canada.ca/en/transparency/privacy.html'
+      privacyUrl: 'https://www.canada.ca/en/transparency/privacy.html',
+      privacyUsers: 'Privacy',
+      privacyUsersUrl: '/en/users/privacy/',
+      privacyPartners: 'Privacy',
+      privacyPartnersUrl: '/en/partners/privacy/'
     }
   },
   fr: {
@@ -31,7 +35,7 @@ module.exports = {
       termsUsersUrl: '/fr/utilisateurs/avis/',
       termsPartnersUrl: '/fr/partenaires/avis/',
       privacyPartnersUrl: '/fr/partenaires/confidentialite/',
-      contactUsersUrl: '/fr/utilisateurs/contactez-nous/'
+      contactUsersUrl: '/fr/utilisateurs/contactez-nous/',
       contactPartnersUrl: 'https://forms-formulaires.alpha.canada.ca/fr/id/cmfmxu3xh0000x401zperejwc'
     },  
     sub: {
@@ -40,7 +44,11 @@ module.exports = {
       terms: 'Avis', 
       termsUrl: 'https://www.canada.ca/fr/transparence/avis.html',
       privacy: 'Confidentialité',
-      privacyUrl: 'https://www.canada.ca/fr/transparence/confidentialite.html'
+      privacyUrl: 'https://www.canada.ca/fr/transparence/confidentialite.html',
+      privacyUsers: 'Confidentialité',
+      privacyUsersUrl: '/fr/utilisateurs/confidentialite/',
+      privacyPartners: 'Confidentialité',
+      privacyPartnersUrl: '/fr/partenaires/confidentialite/'
     }
   },
 };
