3d973845510f9817ca9c73a23958eb1b062754e8
sukhichuhan-cds <233160570+sukhichuhan-cds@users.noreply.github.com>
2025-12-18 11:20:05 -0500

    Updated the contextual band on both user and partner footers

--- website/src/_data/footer.js
+++ website/src/_data/footer.js
@@ -3,11 +3,14 @@ module.exports = {
     contextual: {
       heading: 'GC Sign in',
       aboutUsersUrl: '/en/users/what-is-gc-sign-in/',
+      aboutPartnersUrl: '/en/partners/about-gc-sign-in/',
       usersUrl: '/en/users/',
       partnersUrl: '/en/partners/',
       termsUsersUrl: '/en/users/terms/',
       termsPartnersUrl: '/en/partners/terms-and-conditions/',
       privacyPartnersUrl: '/en/partners/privacy/',
+      contactUsersUrl: '/en/users/contact-us/'
+      contactPartnersUrl: 'https://forms-formulaires.alpha.canada.ca/en/id/cmfmxu3xh0000x401zperejwc'
     },
     sub: {
       about: 'About Canada.ca',
@@ -22,12 +25,15 @@ module.exports = {
     contextual: {
       heading: 'Connexion GC',
       aboutUsersUrl: '/fr/utilisateurs/a-propos/',
+      aboutPartnersUrl: '/fr/partenaires/a-propos-de-connexion-gc/',
       usersUrl: '/fr/utilisateurs/',
       partnersUrl: '/fr/partenaires/',
       termsUsersUrl: '/fr/utilisateurs/avis/',
       termsPartnersUrl: '/fr/partenaires/avis/',
       privacyPartnersUrl: '/fr/partenaires/confidentialite/',
-    },
+      contactUsersUrl: '/fr/utilisateurs/contactez-nous/'
+      contactPartnersUrl: 'https://forms-formulaires.alpha.canada.ca/fr/id/cmfmxu3xh0000x401zperejwc'
+    },  
     sub: {
       about: 'À propos de Canada.ca',
       aboutUrl: 'https://www.canada.ca/fr/gouvernement/a-propos-canada-ca.html',
