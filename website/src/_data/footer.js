8e1c22a3886ac27fab2425dc41a20d54810fbb5e
sukhichuhan-cds <233160570+sukhichuhan-cds@users.noreply.github.com>
2025-12-19 11:27:32 -0500

    Sukhi/users-partners/footer-update (#546)
    * Added privacy notice statement in both english and french
    
    * minor update on english privacy page
    
    * Updated the contextual band on both user and partner footers
    
    * Updated sub-links
    
    * Update footer.js
    
    * Update footer.njk
    
    * Update footer.js
    
    ---------
    
    Co-authored-by: amalungo <afonso.malungo@cds-snc.ca>

--- website/src/_data/footer.js
+++ website/src/_data/footer.js
@@ -3,38 +3,60 @@ module.exports = {
     contextual: {
       heading: 'GC Sign in',
       aboutUsersUrl: '/en/users/what-is-gc-sign-in/',
+      aboutPartnersUrl: '/en/partners/about-gc-sign-in/',
       usersUrl: '/en/users/',
       partnersUrl: '/en/partners/',
       termsUsersUrl: '/en/users/terms/',
       termsPartnersUrl: '/en/partners/terms-and-conditions/',
       privacyPartnersUrl: '/en/partners/privacy/',
+      contactUsersUrl: '/en/users/contact-us/',
+      contactPartnersUrl: 'https://forms-formulaires.alpha.canada.ca/en/id/cmfmxu3xh0000x401zperejwc'
     },
     sub: {
       about: 'About Canada.ca',
       aboutUrl: 'https://www.canada.ca/en/government/about-canada-ca.html',
       terms: 'Terms and conditions',
       termsUrl: 'https://www.canada.ca/en/transparency/terms.html',
+      termsUsers: 'Terms and conditions',
+      termsUsersUrl: '/en/users/terms/',
+      termsPartners: 'Terms and conditions',
+      termsPartnersUrl: '/en/partners/terms-and-conditions/',
       privacy: 'Privacy',
-      privacyUrl: 'https://www.canada.ca/en/transparency/privacy.html'
+      privacyUrl: 'https://www.canada.ca/en/transparency/privacy.html',
+      privacyUsers: 'Privacy',
+      privacyUsersUrl: '/en/users/privacy/',
+      privacyPartners: 'Privacy',
+      privacyPartnersUrl: '/en/partners/privacy/'
     }
   },
   fr: {
     contextual: {
       heading: 'Connexion GC',
       aboutUsersUrl: '/fr/utilisateurs/a-propos/',
+      aboutPartnersUrl: '/fr/partenaires/a-propos-de-connexion-gc/',
       usersUrl: '/fr/utilisateurs/',
       partnersUrl: '/fr/partenaires/',
       termsUsersUrl: '/fr/utilisateurs/avis/',
       termsPartnersUrl: '/fr/partenaires/avis/',
       privacyPartnersUrl: '/fr/partenaires/confidentialite/',
-    },
+      contactUsersUrl: '/fr/utilisateurs/contactez-nous/',
+      contactPartnersUrl: 'https://forms-formulaires.alpha.canada.ca/fr/id/cmfmxu3xh0000x401zperejwc'
+    },  
     sub: {
       about: 'À propos de Canada.ca',
       aboutUrl: 'https://www.canada.ca/fr/gouvernement/a-propos-canada-ca.html',
       terms: 'Avis', 
       termsUrl: 'https://www.canada.ca/fr/transparence/avis.html',
+      termsUsers: 'Avis',
+      termsUsersUrl: '/fr/utilisateurs/avis/',
+      termsPartners: 'Avis',
+      termsPartnersUrl: '/fr/partenaires/avis/',
       privacy: 'Confidentialité',
-      privacyUrl: 'https://www.canada.ca/fr/transparence/confidentialite.html'
+      privacyUrl: 'https://www.canada.ca/fr/transparence/confidentialite.html',
+      privacyUsers: 'Confidentialité',
+      privacyUsersUrl: '/fr/utilisateurs/confidentialite/',
+      privacyPartners: 'Confidentialité',
+      privacyPartnersUrl: '/fr/partenaires/confidentialite/'
     }
   },
 };
