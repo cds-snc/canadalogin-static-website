4bf4c20e79c5aecc6b093937ac455dd0acc3e1a8
mahamoud mohamoud <mahamoud.mohamoud@cds-snc.ca>
2026-03-13 15:00:03 +0000

    refactor

--- website/.eleventy.js
+++ website/.eleventy.js
@@ -13,12 +13,12 @@ const svgContents = require('eleventy-plugin-svg-contents');
 const codeClipboard = require('eleventy-plugin-code-clipboard');
 const { getLatestCdnVersion } = require('./utils/cdn-info');
 const { DateTime } = require('luxon');
-const cheerio = require('cheerio');
 
 const contextMenu = require('./utils/context-menu');
 const markdownAnchor = require('./utils/anchor');
 const slugify = require('./utils/slugify');
 const { encode } = require('html-entities');
+const { gcdsTransform } = require('./utils/gcds-transform');
 
 module.exports = function (eleventyConfig) {
   // Pass through copies
@@ -326,166 +326,7 @@ module.exports = function (eleventyConfig) {
   });
 
   eleventyConfig.addTransform("gcdsTransform", function (content) {
-    if (!this.outputPath || !this.outputPath.endsWith(".html")) {
-      return content;
-    }
-
-    const articlesApiUrl = process.env.ARTICLES_API;
-    const dom = cheerio.load(content);
-
-    function normalizeHref(href) {
-      if (articlesApiUrl && href?.startsWith(articlesApiUrl)) {
-        return href.replace(articlesApiUrl, "");
-      }
-      return href;
-    }
-
-    function isExternalElement(element) {
-      const el = dom(element);
-      const rel = (el.attr("rel") || "").split(/\s+/);
-      return rel.includes("noreferrer") || rel.includes("noopener");
-    }
-
-    // Convert details.alert blocks to gcds-notice
-    dom("details.alert").each(function () {
-      const details = dom(this);
-
-      const classes = details.attr("class") || "";
-      const typeMatch = classes.match(/alert-([^\s]+)/);
-      const type = encode(typeMatch ? typeMatch[1] : "info");
-      const title = encode(details.find("summary h3").text().trim());
-
-      const bodyContent = details
-        .clone()
-        .children("summary")
-        .remove()
-        .end()
-        .html()
-        .trim();
-
-      const notice = dom(`
-      <section class="mt-300 mb-300">
-        <gcds-notice type="${type}" notice-title-tag="h2" notice-title="${title}">
-          <gcds-text>${bodyContent}</gcds-text>
-        </gcds-notice>
-      </section>
-    `);
-
-      details.replaceWith(notice);
-    });
-
-    // Convert wp-block-details blocks to gcds-details
-    dom("details.wp-block-details").each(function () {
-      const details = dom(this);
-
-      const title = encode(details.find("summary").first().text().trim());
-
-      const bodyContent = details
-        .clone()
-        .children("summary")
-        .remove()
-        .end()
-        .html()
-        .trim();
-
-      const gcdsDetails = dom(`<gcds-details details-title="${title}">${bodyContent}</gcds-details>`);
-
-      details.replaceWith(gcdsDetails);
-    });
-
-    // Convert Gutenberg button blocks to gcds-button (must run before anchor transform)
-    const BUTTON_ROLES = ["secondary", "start", "danger"];
-
-    dom(".wp-block-button").each(function () {
-      const wrapper = dom(this);
-      const anchor = wrapper.find("a.wp-block-button__link");
-
-      if (!anchor.length) return;
-
-      const href = anchor.attr("href");
-      const target = anchor.attr("target") || "_self";
-      const label = anchor.html();
-      const displayHref = normalizeHref(href);
-
-      const wrapperClasses = (wrapper.attr("class") || "").split(/\s+/);
-      const anchorClasses = (anchor.attr("class") || "").split(/\s+/);
-      const allClasses = [...wrapperClasses, ...anchorClasses];
-      const buttonRole = allClasses.find((cls) => BUTTON_ROLES.includes(cls));
-      const hasChevron = allClasses.includes("chevron-right");
-
-      const buttonLabel = hasChevron
-        ? `${label}&nbsp;<gcds-icon name="chevron-right"></gcds-icon>`
-        : label;
-
-      const gcdsButton = dom("<gcds-button></gcds-button>")
-        .attr("type", "link")
-        .attr("value", label)
-        .attr("href", displayHref)
-        .attr("target", target)
-        .html(buttonLabel);
-
-      if (buttonRole) {
-        gcdsButton.attr("button-role", buttonRole);
-      }
-
-      wrapper.replaceWith(gcdsButton);
-    });
-
-    // Convert remaining anchor links to gcds-link
-    dom("a").each(function () {
-      const anchor = dom(this);
-
-      // Skip anchors already inside gcds components
-      if (anchor.closest("gcds-button").length) return;
-
-      const href = anchor.attr("href");
-      const displayHref = normalizeHref(href);
-      const label = anchor.html();
-
-      const gcdsLink = dom("<gcds-link></gcds-link>")
-        .attr("href", displayHref)
-        .html(label);
-
-      if (isExternalElement(anchor)) {
-        gcdsLink.attr("external", "");
-        gcdsLink.attr("target", "_blank");
-      }
-
-      anchor.replaceWith(gcdsLink);
-    });
-
-    // Convert wp-block-list classes to utility classes
-    dom("ul.wp-block-list").each(function () {
-      dom(this).removeClass("wp-block-list").addClass("list-disc");
-    });
-
-    dom("ol.wp-block-list").each(function () {
-      dom(this).removeClass("wp-block-list").addClass("list-decimal");
-    });
-
-    // Convert wp-block-columns to gcds-grid
-    dom(".wp-block-columns").each((_, columnsBlock) => {
-      const columns = dom(columnsBlock).find(".wp-block-column");
-      const columnCount = columns.length;
-
-      const desktopColumns = Array(columnCount).fill("1fr").join(" ");
-      const tabletColumns = columnCount > 2 ? "1fr 1fr" : desktopColumns;
-
-      const grid = dom("<gcds-grid></gcds-grid>")
-        .attr("columns-desktop", desktopColumns)
-        .attr("columns-tablet", tabletColumns)
-        .attr("columns", "1fr");
-
-      columns.each((_, column) => {
-        const columnContent = dom(column).html();
-        const gridColumn = dom("<div></div>").html(columnContent);
-        grid.append(gridColumn);
-      });
-
-      dom(columnsBlock).replaceWith(grid);
-    });
-
-    return dom.html();
+    return gcdsTransform(content, this.outputPath);
   });
 
   return {
