93bed30b9aae85790bac1788d136a4df13bc8eba
mahamoud mohamoud <mahamoud.mohamoud@cds-snc.ca>
2026-03-16 21:54:54 +0000

    feat: data api integration for articles api support

---
+++ website/src/_data/articles_fr.js
@@ -0,0 +1,29 @@
+module.exports = async function () {
+    try {
+        const ARTICLES_API_URL = process.env.ARTICLES_API;
+        console.log("ARTICLES_API_URL", ARTICLES_API_URL);
+        console.log("Fetching FR articles from Articles API");
+
+        const res = await fetch(
+            `${ARTICLES_API_URL}/wp-json/wp/v2/pages?per_page=100&lang=fr`
+        );
+
+        if (!res.ok) {
+            throw new Error(`Articles FR API error: ${res.status}`);
+        }
+        const pages = await res.json();
+
+        const bySlug = {};
+
+        pages.forEach(page => {
+            bySlug[page.slug] = page;
+        });
+        console.log("French Articles by slug:", Object.keys(bySlug));
+        return bySlug;
+
+    } catch (error) {
+        console.error("Error fetching French articles:", error);
+        throw error;
+    }
+
+};
